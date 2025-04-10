1f013dfb5ad872847b2427aa340c98dc11a007cc
Lesley Solorzano <lsolorzano@appdev.yearup.org>
2025-04-10 11:32:05 -0500

    added shopping list items

---
+++ shopping-list/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("Shoping list\n--------------\nChicken breast\nTomatoes\nOnions\nJalapenos\nJuice");
+        System.out.println("Milk");
+        System.out.println("Cereal");
+        System.out.println("Bread");
+        System.out.println("Oatmeal");
+        System.out.println("Ground Beef");
+
+    }
+}
