65b081902f7ce5d8dd5d545acdf4bcef0c1b8eda
Lesley Solorzano <lsolorzano@appdev.yearup.org>
2025-04-15 10:12:17 -0500

    Completed Mod4 - Exercise 1 - Sandwich Shop

---
+++ SanwichShop/src/main/java/com/pluralsight/SanwichShop.java
@@ -0,0 +1,41 @@
+package com.pluralsight;
+import java.util.Scanner;
+public class SanwichShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt fdr sandwich size
+        System.out.println("Welcome to the Sandwich Shop!");
+        System.out.println("Enter Sanwich size (1 for Regular - $5.45, 2 for Large - $8.95): " );
+        int size = scanner.nextInt();;
+
+        //Get Price
+        double basePrice;
+        if (size == 1) {
+            basePrice = 5.45;
+        } else if (size ==2) {
+            basePrice = 8.95;
+        } else {
+            System.out.println("Invalid size selection. Please enter 1 or 2.");
+            return;
+        }
+        // Prompt for age
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        // Calculate discount
+        double discount = 0.0;
+        if (age <= 17) {
+            discount = 0.10;
+        } else if (age >= 65) {
+            discount = 0.20;
+
+        }
+        // Calculate final price
+        double finalPrice = basePrice - (basePrice * discount);
+        // Display the cost
+        System.out.printf("Your sandwich costs: $%.2f%n", finalPrice);
+
+        scanner.close();
+    }
+}
